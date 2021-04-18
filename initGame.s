e960c55f366ae316e362e786c8fb0004540ef9fe
Sean <82683346+SResor@users.noreply.github.com>
2021-04-18 05:28:07 -0700

    Update initGame.s
    Basics of the initGame.s function

--- initGame.s
+++ initGame.s
@@ -14,9 +14,29 @@
 .type initGame, %function
 
 initGame:
-push	{fp, lr}		@ Pushes fp and lr onto the stack
+	push	{fp, lr}		@ Pushes fp and lr onto the stack
    	add 	fp, sp, #4		@ Adds 4 to sp and stores it in fp
 
-	mov	r0, #0			@ Stores #0 into r0
+	mov	r4, #0			@ Moves #0 into r4
+	mov	r5, #0			@ Moves #0 into r5
 
+loop1:
+	cmp	r4, 46			@ Compares the value stored in r4 to 46
+	bge	outloop1		@ If r4 is greater than or equal to 46, exits loop
+	mov	r0, #0			@ Stores #0 into r0
 	bl	rand			@ Generates random number
+	sub	r2, #46, r4		@ Subtracts r4 from 46 and moves it into r5
+	bl	modulo			@ Calls the modulo.s function
+	
+	add	r0, r0, #1		@ Adds 1 to the number returned by modulo.s
+	str	r0, [r10, r5]		@ Stores the number returned by modulo.s into the approriate array address
+	ldr	r6, [r9]		@ Loads the number of piles counter into r6
+	add	r6, r6, #1		@ Increments the number of piles by 1
+	str	r6, [r9]		@ Stores the new value in r6 into the address that holds the number of piles
+	add	r5, r5, #4		@ Adds 4 into r5 and moves it back into r5
+	bl	loop1			@ Branches back to loop1
+	
+outloop1:
+
+	sub 	sp, fp, #4		@ Moves down one memory location from fp and stores it in sp
+	pop 	{fp, pc}		@ Pops fp and pc from the stack
