7d6e50be4fb9d59498306abcf9241db39ae7083f
Sean <82683346+SResor@users.noreply.github.com>
2021-04-19 02:57:00 -0700

    Create checkPiles.s

---
+++ checkPiles.s
@@ -0,0 +1,20 @@
+@ checkPiles.s
+@ This function checks whether any of the piles have no cards left and
+ @ condenses the array accordingly so all piles are in contiguous memory addresses
+
+.cpu cortex-a53
+.fpu neon-fp-armv8
+
+.data
+
+.text
+.align 2
+.global checkPiles
+.syntax unified
+.type checkPiles, %function
+
+checkPiles:
+	push	{fp, lr}		@ Pushes fp and lr onto the stack
+	add 	fp, sp, #4		@ Adds 4 to sp and stores it in fp
+	
+  
