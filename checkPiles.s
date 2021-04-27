b4361ff5c905629bb8023a22c2df51d2a522dce6
Sean <82683346+SResor@users.noreply.github.com>
2021-04-26 20:26:43 -0700

    Update checkPiles.s

--- checkPiles.s
+++ checkPiles.s
@@ -17,37 +17,40 @@ checkPiles:
 	push	{fp, lr}		@ Pushes fp and lr onto the stack
 	add 	fp, sp, #4		@ Adds 4 to sp and stores it in fp
 	
-	mov	r4, #0			@ Moves 0 into r4, our iterator register
-	mov	r5, #0			@ Moves 0 into r5, our iterator address location register
-	ldr	r6, [r9]		@ Loads the number of card piles into r6
-	mov	r7, r6, LSL #2		@ Moves r6 * 4 into r7, the address where the last array index is stored
-	mov	r2, #0			@ Moves 0 into r2
+	mov	r4, #0			@ Moves #0 into r4 (our iterator)
+	ldr	r6, [r9]		@ Moves the number of piles into r6
 	
-loop1:
-	cmp	r4, r6			@ Compares r4 to number of piles
-
-	bge	outloop1		@ If r4 is greater than or equal to number of piles, exit loop
-
-  ldr	r0, [r10, r5]		@ Loads the number stored in the relevant array index address into r0
-	cmp	r0, #0			@ Compares r0 to 0
-	bne	outloop2		@ If they are not equal, exits the loop
-	ldr	r1, [r10, r7]		@ Loads the value at the end of the array into r1
-	str	r2, [r10, r7]		@ Places the value in r2 at the end of the array
-	str	r1, [r10, r5]		@ Stores the value in r1 into the array index address being considered
-	sub	r4, r4, #1		@ Increments r4 by 1, our iterator
-	sub	r5, r5, #4		@ Increments r4 by 4, our iterator address location register
-	sub	r6, r6, #1		@ Decrements r6 by 1, the number of card piles
-	sub	r7, r7, #4		@ Decrements r7 by 4, the address where the last array index is stored
+while:
+	cmp	r4, r6			@ Compares the iterator (r4) to the numbers of piles
+	bge	endWhile
 	
-	outloop2:
-	add	r4, r4, #1		@ Increments r4 by 1, our iterator
-	add	r5, r5, #4		@ Increments r4 by 4, our iterator address location register
-
-  bl	loop1			@ Branches back to the start of the loop
+	mov	r5, r4, LSL #2		@ Moves proper array address offset into r5
+	ldr	r3, [r10, r5]		@ Moves the array index value into r3
 	
-outloop1:
-	add	r6, r6, #1
-	str	r6, [r9]		@ Stores the value in r6 into the address in r9
+	cmp	r3, #0			@ Compares r3 to #0
+	bne	skipSwap		@ If they're not equal, skips the swapping code
+	sub	r6, r6, #1		@ Decrements the number of piles by 1
+	mov	r3, r6, LSL #2		@ Moves the address of the last array index into r3
+	ldr	r3, [r10, r3]		@ Moves the value held in the last array index into r3
+	str	r3, [r10, r5]		@ Replaces the zero in the array with the value in r3
+	
+skipSwap:
+	add	r4, r4, #1		@ Increments the iterator (r4) by one
+	bl	while			@ Branches back to while
+	
+endWhile:
+	cmp	r4, r6			@ Compares the iterator (r4) to the number of piles (r6)
+	bne	doneRemoveZero		@ If they're not equal, skips code
+	
+	mov	r5, r4, LSL #2		@ Stores the address of the array index into r5
+	ldr	r5, [r10, r5]		@ Stores the value at the proper array index into r5
+	cmp	r5, #0			@ Compares the value in r5 to #0
+	bne	doneRemoveZero
+	
+	sub	r6, r6, #1		@ Decrements r6 by 1
+	
+doneRemoveZero:
+	str	r6, [r9]		@ Stores the number of piles into the address in r9
 	
 	sub 	sp, fp, #4		@ Moves down one memory location from fp and stores it in sp
 	pop 	{fp, pc}		@ Pops fp and pc from the stack
